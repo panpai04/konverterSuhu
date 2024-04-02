e523855e04c07efca5cf8742ea4fbff60a7fb1a9
panpai04 <irpan.pauzi16@gmail.com>
2024-04-02 14:01:31 +0700

    cb

--- model/dbConnection.js
+++ model/dbConnection.js
@@ -1,7 +1,7 @@
 const pg = require('pg');
 const client = new pg.Client({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    // port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD
